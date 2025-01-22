461e36cdbd600afb645916167a648c8ea5766099
José Inacio <inaciojosea13@gmail.com>
2025-01-22 21:33:45 +0100

    create cohort module, first try

--- server/models/Cohort.model.js
+++ server/models/Cohort.model.js
@@ -1,6 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const cohortSchema = new Schema(
+	{
+		cohortSlug: String,
+		cohortName: String,
+		program: String,
+		format: String,
+		campus: String,
+		startDate: Date,
+		endDate: Date,
+		inProgress: Boolean,
+		programManager: String,
+		leadTeacher: String,
+		totalHours: Number
+	},
+	{ timestamps: true }
+);
+
+const Cohort = mongoose.model("Cohort", cohortSchema)
+
+module.exports = Cohort;
+
 
 //Example from lesson:
 /* 
