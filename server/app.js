7f0fdbb2ee537e5fefc23e3aa652b2043ef17734
Andrea Bronzoni <bronzoni.andrea@libero.it>
2025-01-25 15:03:33 +0100

    cors installed

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const cookieParser = require("cookie-parser");
 const Student = require("./models/Student.model");
 const Cohort = require("./models/Cohort.model");
 const PORT = 5005;
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -35,6 +36,7 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors("*"));
 
 // ROUTES - https://expressjs.com/en/starter/basic-routing.html
 // Devs Team - Start working on the routes here:
