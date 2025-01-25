642f4e2c993ae4416cef87aec4c2d4007ddea011
Sven Andersson <sven@andersson.es>
2025-01-25 13:58:49 +0100

    finished the student model

--- server/models/Student.model.js
+++ server/models/Student.model.js
@@ -1,34 +1,64 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-
-const studentSchema = new Schema (
+const studentSchema = new Schema(
 	{
-		firstName: String, 
-		lastName: String, 
-		email: String, 
-		phone: String, 
-		linedinUrl: String, 
-		languages: {
-			type: Array, 
-			enum: ["English", "Spanish", "French", "German", "Portuguese", "Dutch", "Other"]
+		firstName: {
+			type: String,
+			required: true,
 		},
-		program: String, 
-		background: String, 
-		image: String, 
+		lastName: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			unique: true,
+			required: true,
+		},
+		phone: {
+			type: String,
+			required: true,
+		},
+		linkedinUrl: {
+			type: String,
+			default: "",
+		},
+		languages: {
+			type: [String],
+			enum: [
+				"English",
+				"Spanish",
+				"French",
+				"German",
+				"Portuguese",
+				"Dutch",
+				"Other",
+			],
+		},
+		program: {
+			type: String,
+			enum: ["Web Dev","UX/UI","Data Analytics","Cybersecurity"]
+		},
+		background: {
+			type: String,
+			default: "",
+		},
+		image: {
+			type: String,
+			default: "https://i.imgur.com/r8bo8u7.png",
+		},
 		cohort: {
 			type: Schema.Types.ObjectId,
-			ref: "Cohort"
+			ref: "Cohort",
 		},
-		projects: Array, 
-
-}, 
-	{timestamp: true}
-)
+		projects: [String],
+	},
+	{ timestamp: true }
+);
 
 //CREATE MODEL
-const Student = mongoose.model("Student", studentSchema)
+const Student = mongoose.model("Student", studentSchema);
 
 // EXPORT THE MODEL
 module.exports = Student;
